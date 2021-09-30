c57a39b4d777d399721fb5b652d45e078ded0cb3
Reihan <reihanyasfinu@gmail.com>
2021-09-30 09:16:43 +0700

    first commit

---
+++ src/main/java/com/Project1/Project1Market/controllers/MainController.java
@@ -0,0 +1,16 @@
+package com.Project1.Project1Market.controllers;
+
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class MainController {
+
+    @RequestMapping("/")
+    public String index() {
+        return "test";
+    }
+    
+    
+}
