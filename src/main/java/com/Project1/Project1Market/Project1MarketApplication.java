f2c936bfd49db6d7eef9faf547177dfdaf70e07a
mellntus <87801994+mellntus@users.noreply.github.com>
2021-10-01 19:32:37 +0700

    ini yang kedua

---
+++ src/main/java/com/Project1/Project1Market/Project1MarketApplication.java
@@ -0,0 +1,13 @@
+package com.Project1.Project1Market;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Project1MarketApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Project1MarketApplication.class, args);
+	}
+
+}
