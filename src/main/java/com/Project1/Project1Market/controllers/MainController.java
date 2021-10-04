18172ecf69c96761ffbb7c5d18c4b8e51b75da34
Reihan <reihanyasfinu@gmail.com>
2021-10-04 14:31:34 +0700

    Access profile

--- src/main/java/com/Project1/Project1Market/controllers/MainController.java
+++ src/main/java/com/Project1/Project1Market/controllers/MainController.java
@@ -2,6 +2,7 @@ package com.Project1.Project1Market.controllers;
 
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -12,5 +13,10 @@ public class MainController {
         return "index";
     }
     
+    @GetMapping("/profile")
+    public String profile() {
+
+        return "profile";
+    }
     
 }
